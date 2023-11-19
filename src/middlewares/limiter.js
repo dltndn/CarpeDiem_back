cf7815d86c1ab82ddbbb269770b5bc0cc6b1c8dd
dltndn <james98099@gmail.com>
2023-11-19 14:36:14 +0900

    fix: limiter x-forwarded-for client ip불러오는 문제 수정

--- src/middlewares/limiter.js
+++ src/middlewares/limiter.js
@@ -1,9 +1,16 @@
 const rateLimit = require('express-rate-limit');
 
+const getIP = request =>
+  request.ip ||
+  request.headers['x-forwarded-for'] ||   
+  request.headers['x-real-ip'] ||
+  request.connection.remoteAddress
+
 // Rate limiter 설정
 const limiter = rateLimit({
   windowMs: 3 * 1000, // 1 seconds
   max: 20, // limit each IP to 50 requests per windowMs
+  keyGenerator: getIP,
   handler(_, res) {
     res.status(429).json({ errorMsg: 'Too many requests' });
   },
