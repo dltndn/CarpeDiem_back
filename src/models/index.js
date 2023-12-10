322a58c0e50fac7cf7fa49810a86841160d3fd69
dltndn <james98099@gmail.com>
2023-12-10 16:55:16 +0900

    feat: 각 라우터 요청 횟수 저장 코드 추가

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,4 @@
 module.exports.User = require('./user.model');
 module.exports.Games = require('./game.model')
 module.exports.UserGameId = require('./userGameId.model')
+module.exports.ManagementDb = require('./managementDb.model')
--- src/routes/games.route.js
+++ src/routes/games.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const validate = require('../middlewares/validate');
-const { gamesController } = require('../controllers');
+const { gamesController, managemetController } = require('../controllers');
 // const { userValidation } = require('../validations');
 const { isUserAuthenticated } = require('../middlewares/authorization')
 const { isValidUserRefreshToken } = require('../middlewares/authorization')
@@ -9,13 +9,13 @@ const router = express.Router();
 
 
 router.route('/').post(gamesController.test);
-router.route('/topWinnersMini').get(gamesController.getTopWinnersMini)
-router.route('/topWinners').post(isValidUserRefreshToken, gamesController.getTopWinners)
-router.route('/userGames').post(isValidUserRefreshToken, gamesController.getUserGames)
-router.route('/updateRewardClaimed').post(isValidUserRefreshToken, gamesController.updateClaimRewards)
-router.route('/currentGames').post(gamesController.getCurrentGames)
-router.route('/getGamesByIds').post(gamesController.getGamesByIds)
-router.route('/userTotalRewards').post(isValidUserRefreshToken, gamesController.getUserTotalRewards)
+router.route('/topWinnersMini').get(managemetController.increaseRoute('topWinnersMini'), gamesController.getTopWinnersMini)
+router.route('/topWinners').post(managemetController.increaseRoute('topWinners'), isValidUserRefreshToken, gamesController.getTopWinners)
+router.route('/userGames').post(managemetController.increaseRoute('userGames'), isValidUserRefreshToken, gamesController.getUserGames)
+router.route('/updateRewardClaimed').post(managemetController.increaseRoute('updateRewardClaimed'), isValidUserRefreshToken, gamesController.updateClaimRewards)
+router.route('/currentGames').post(managemetController.increaseRoute('currentGames'), gamesController.getCurrentGames)
+router.route('/getGamesByIds').post(managemetController.increaseRoute('getGamesByIds'), gamesController.getGamesByIds)
+router.route('/userTotalRewards').post(managemetController.increaseRoute('userTotalRewards'), isValidUserRefreshToken, gamesController.getUserTotalRewards)
 
 
 module.exports = router;
